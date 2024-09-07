b5d6778a926b05f9667aafb2a96ed162b28fd90b
RIGHTGAMER <100288505+RIGHTGAMER@users.noreply.github.com>
2024-09-07 19:24:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Obeying NimOn","Playing DRP"];
 
 
 let currentIndex = 0;
